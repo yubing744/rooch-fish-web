f1fbc80d41f8f29cceea524267d1433650aefd29
Owen Wu <yubing744@163.com>
2024-11-28 06:30:34 +0800

    feat: update network to mainnet

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,22 +1,342 @@
 export const config = {
   debug: false,
-  network: "testnet", // localnet, testnet
-  roochFishAddress: "0xb38a327121ab8e9091a04377ec1e9af9ab4b801dbfb368f20fb0c080c763f7e8",
-  gameStateObjectID: "0x3bbe1056fcfdfee92eae4d7045d9a231a3d5a4ca7ddd77e43d730bc80d8bc4d8",
+  network: "mainnet", // localnet, testnet
+  roochFishAddress: "0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b",
+  gameStateObjectID: "0x2bfb5ec4c7ff158a977825100fa8e28732c6f6bf04e16e6f9cbbfaaf9ad6c15a",
   ponds: {
-    0: "0x4afb602f8bbbb516ef9c8dbcecb8d91bfb5ddb618a7505da0abb1904ebc1784b",
-    1: "0x1fd948251d394db82f9d4a1e82118cf232c0f4f9c2c7ffa45ea9dffad28e9412",
-    2: "0x676cb4ef030fd3cfc4a151a348e6c9bc987f97a11586dcee5660cd6bb1a2047c",
+    0: "0xab4f4a7f09bd03d149ae2114310403239103b811da693909ccbfd97a88c685cc",
+    1: "0x3f10b4aeb367801d085a02a173a3c1952232e70dd52feee71de5d05b0b3d317f",
+    2: "0x5dc93eff6444fe9c8a95d8108bb661bfa1eb45949647a09056d66e2bd3d96a97",
     3: "0x14b8edb38062592b47b6898e83c7b0df8f1d4439c0fb9d6728435ef870516b15",
-    4: "0x2b3d3dc14e62060099e49a8c522b5698219795bf6a87d42624cc69eaf6263627",
-    5: "0x7fe629442f293a0307c7cc6ae12edc0c60c4a7708c75a83b5f671ff03544e781",
-    6: "0x855d118b9da7ac1f32922cf568f51d9df1e28cd0205d9f241ba0007c22fea5bb",
-    7: "0xac8652636629f6bfe5ac6e102308bc032b01101243e50a3604c3d12955abf179"
+    4: "0x0c08a409ba6d92bd1ca8c5fd7adf1413b2dff5eb7dbf5129ef57a4326abc5dab",
+    5: "0x3c671e925d9b0e92efd2b120896690feb053364f62d76f9cdc0bdf5414ba032b",
+    6: "0xe964216c2a68c05c249c7f9d779511db9e5afa4e60de6b7e4c8fb4940d973a75",
+    7: "0x7a7de52e29d34416c45b374ef7518901adae411f7a411d4beb541975e0f63c73"
   },
   publicURL: `${import.meta.env.VITE_PUBLIC_URL}`,
 }
 
 export type PondID = keyof typeof config.ponds;
+
+/*
+mainnet
+
+New modules:
+    0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::simple_rng
+    0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond
+    0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::fish
+    0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::player
+    0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::quad_tree
+    0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::rooch_fish
+    0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::utils
+    0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::food
+
+gameworld:
+[
+  {
+    "id": "0x2bfb5ec4c7ff158a977825100fa8e28732c6f6bf04e16e6f9cbbfaaf9ad6c15a",
+    "owner": "rooch1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqhxqaen",
+    "owner_bitcoin_address": null,
+    "flag": 1,
+    "state_root": "0x5350415253455f4d45524b4c455f504c414345484f4c4445525f484153480000",
+    "size": "0",
+    "created_at": "1732745942851",
+    "updated_at": "1732745942851",
+    "object_type": "0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::rooch_fish::GameState",
+    "value": "0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b0148954635d648a30c92751ebff4a6ee54bd8edfa3a97e7103c75f5cfd199c27a70122da724b4b0f8396d0395c77546a74312d4680c4e5ebc3179db20c050607756f00000000000000000000000000000000000000000000000000000000000000000000000000000000",
+    "decoded_value": {
+      "abilities": 8,
+      "type": "0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::rooch_fish::GameState",
+      "value": {
+        "admin": "0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b",
+        "player_list": {
+          "abilities": 12,
+          "type": "0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::player::PlayerList",
+          "value": {
+            "player_count": "0",
+            "players": {
+              "abilities": 4,
+              "type": "0x2::table::Table<address, 0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::player::PlayerState>",
+              "value": {
+                "handle": {
+                  "abilities": 12,
+                  "type": "0x2::object::Object<0x2::table::TablePlaceholder>",
+                  "value": {
+                    "id": "0x22da724b4b0f8396d0395c77546a74312d4680c4e5ebc3179db20c050607756f"
+                  }
+                }
+              }
+            },
+            "total_feed": "0"
+          }
+        },
+        "ponds": {
+          "abilities": 4,
+          "type": "0x2::table::Table<u64, 0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>>",
+          "value": {
+            "handle": {
+              "abilities": 12,
+              "type": "0x2::object::Object<0x2::table::TablePlaceholder>",
+              "value": {
+                "id": "0x48954635d648a30c92751ebff4a6ee54bd8edfa3a97e7103c75f5cfd199c27a7"
+              }
+            }
+          }
+        }
+      }
+    },
+    "display_fields": null
+  }
+]
+
+ponds:
+{
+  "data": [
+    {
+      "field_key": "0x11228d102ec1ccd0e71d31a6115c33b886a4cd5cb48113db5c851c6d96e82b7f",
+      "state": {
+        "id": "0x48954635d648a30c92751ebff4a6ee54bd8edfa3a97e7103c75f5cfd199c27a711228d102ec1ccd0e71d31a6115c33b886a4cd5cb48113db5c851c6d96e82b7f",
+        "owner": "rooch1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqhxqaen",
+        "owner_bitcoin_address": null,
+        "flag": 0,
+        "state_root": "0x5350415253455f4d45524b4c455f504c414345484f4c4445525f484153480000",
+        "size": "0",
+        "created_at": "1732745942851",
+        "updated_at": "1732745942851",
+        "object_type": "0x2::object::DynamicField<u64, 0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>>",
+        "value": "0x0700000000000000017a7de52e29d34416c45b374ef7518901adae411f7a411d4beb541975e0f63c73",
+        "decoded_value": {
+          "abilities": 12,
+          "type": "0x2::object::DynamicField<u64, 0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>>",
+          "value": {
+            "name": "7",
+            "value": {
+              "abilities": 12,
+              "type": "0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>",
+              "value": {
+                "id": "0x7a7de52e29d34416c45b374ef7518901adae411f7a411d4beb541975e0f63c73"
+              }
+            }
+          }
+        },
+        "display_fields": null
+      }
+    },
+    {
+      "field_key": "0x14e41f37bef443dbf4163625571e6d035763b553cad649387b41be8f7c4c7569",
+      "state": {
+        "id": "0x48954635d648a30c92751ebff4a6ee54bd8edfa3a97e7103c75f5cfd199c27a714e41f37bef443dbf4163625571e6d035763b553cad649387b41be8f7c4c7569",
+        "owner": "rooch1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqhxqaen",
+        "owner_bitcoin_address": null,
+        "flag": 0,
+        "state_root": "0x5350415253455f4d45524b4c455f504c414345484f4c4445525f484153480000",
+        "size": "0",
+        "created_at": "1732745942851",
+        "updated_at": "1732745942851",
+        "object_type": "0x2::object::DynamicField<u64, 0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>>",
+        "value": "0x0200000000000000015dc93eff6444fe9c8a95d8108bb661bfa1eb45949647a09056d66e2bd3d96a97",
+        "decoded_value": {
+          "abilities": 12,
+          "type": "0x2::object::DynamicField<u64, 0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>>",
+          "value": {
+            "name": "2",
+            "value": {
+              "abilities": 12,
+              "type": "0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>",
+              "value": {
+                "id": "0x5dc93eff6444fe9c8a95d8108bb661bfa1eb45949647a09056d66e2bd3d96a97"
+              }
+            }
+          }
+        },
+        "display_fields": null
+      }
+    },
+    {
+      "field_key": "0x28b56697e9fd6fbf8d6513b44cbeb793537ef7bd71dbe03c7a8d5cfc992ff407",
+      "state": {
+        "id": "0x48954635d648a30c92751ebff4a6ee54bd8edfa3a97e7103c75f5cfd199c27a728b56697e9fd6fbf8d6513b44cbeb793537ef7bd71dbe03c7a8d5cfc992ff407",
+        "owner": "rooch1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqhxqaen",
+        "owner_bitcoin_address": null,
+        "flag": 0,
+        "state_root": "0x5350415253455f4d45524b4c455f504c414345484f4c4445525f484153480000",
+        "size": "0",
+        "created_at": "1732745942851",
+        "updated_at": "1732745942851",
+        "object_type": "0x2::object::DynamicField<u64, 0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>>",
+        "value": "0x000000000000000001ab4f4a7f09bd03d149ae2114310403239103b811da693909ccbfd97a88c685cc",
+        "decoded_value": {
+          "abilities": 12,
+          "type": "0x2::object::DynamicField<u64, 0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>>",
+          "value": {
+            "name": "0",
+            "value": {
+              "abilities": 12,
+              "type": "0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>",
+              "value": {
+                "id": "0xab4f4a7f09bd03d149ae2114310403239103b811da693909ccbfd97a88c685cc"
+              }
+            }
+          }
+        },
+        "display_fields": null
+      }
+    },
+    {
+      "field_key": "0x2ce2e7bbb14b9b8d582aaf87c705f424e63373a1bb602deed89e14c95615e4b6",
+      "state": {
+        "id": "0x48954635d648a30c92751ebff4a6ee54bd8edfa3a97e7103c75f5cfd199c27a72ce2e7bbb14b9b8d582aaf87c705f424e63373a1bb602deed89e14c95615e4b6",
+        "owner": "rooch1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqhxqaen",
+        "owner_bitcoin_address": null,
+        "flag": 0,
+        "state_root": "0x5350415253455f4d45524b4c455f504c414345484f4c4445525f484153480000",
+        "size": "0",
+        "created_at": "1732745942851",
+        "updated_at": "1732745942851",
+        "object_type": "0x2::object::DynamicField<u64, 0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>>",
+        "value": "0x0400000000000000010c08a409ba6d92bd1ca8c5fd7adf1413b2dff5eb7dbf5129ef57a4326abc5dab",
+        "decoded_value": {
+          "abilities": 12,
+          "type": "0x2::object::DynamicField<u64, 0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>>",
+          "value": {
+            "name": "4",
+            "value": {
+              "abilities": 12,
+              "type": "0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>",
+              "value": {
+                "id": "0x0c08a409ba6d92bd1ca8c5fd7adf1413b2dff5eb7dbf5129ef57a4326abc5dab"
+              }
+            }
+          }
+        },
+        "display_fields": null
+      }
+    },
+    {
+      "field_key": "0x3bc998c5c75500958ca88ca47f5b654ff2fa97da0c1539b6b4c4c8722975a80f",
+      "state": {
+        "id": "0x48954635d648a30c92751ebff4a6ee54bd8edfa3a97e7103c75f5cfd199c27a73bc998c5c75500958ca88ca47f5b654ff2fa97da0c1539b6b4c4c8722975a80f",
+        "owner": "rooch1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqhxqaen",
+        "owner_bitcoin_address": null,
+        "flag": 0,
+        "state_root": "0x5350415253455f4d45524b4c455f504c414345484f4c4445525f484153480000",
+        "size": "0",
+        "created_at": "1732745942851",
+        "updated_at": "1732745942851",
+        "object_type": "0x2::object::DynamicField<u64, 0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>>",
+        "value": "0x060000000000000001e964216c2a68c05c249c7f9d779511db9e5afa4e60de6b7e4c8fb4940d973a75",
+        "decoded_value": {
+          "abilities": 12,
+          "type": "0x2::object::DynamicField<u64, 0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>>",
+          "value": {
+            "name": "6",
+            "value": {
+              "abilities": 12,
+              "type": "0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>",
+              "value": {
+                "id": "0xe964216c2a68c05c249c7f9d779511db9e5afa4e60de6b7e4c8fb4940d973a75"
+              }
+            }
+          }
+        },
+        "display_fields": null
+      }
+    },
+    {
+      "field_key": "0x5a473312d437bbf8c2690476f7f1df5040ff1f2398d19d7c039858dd3423bbbc",
+      "state": {
+        "id": "0x48954635d648a30c92751ebff4a6ee54bd8edfa3a97e7103c75f5cfd199c27a75a473312d437bbf8c2690476f7f1df5040ff1f2398d19d7c039858dd3423bbbc",
+        "owner": "rooch1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqhxqaen",
+        "owner_bitcoin_address": null,
+        "flag": 0,
+        "state_root": "0x5350415253455f4d45524b4c455f504c414345484f4c4445525f484153480000",
+        "size": "0",
+        "created_at": "1732745942851",
+        "updated_at": "1732745942851",
+        "object_type": "0x2::object::DynamicField<u64, 0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>>",
+        "value": "0x0500000000000000013c671e925d9b0e92efd2b120896690feb053364f62d76f9cdc0bdf5414ba032b",
+        "decoded_value": {
+          "abilities": 12,
+          "type": "0x2::object::DynamicField<u64, 0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>>",
+          "value": {
+            "name": "5",
+            "value": {
+              "abilities": 12,
+              "type": "0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>",
+              "value": {
+                "id": "0x3c671e925d9b0e92efd2b120896690feb053364f62d76f9cdc0bdf5414ba032b"
+              }
+            }
+          }
+        },
+        "display_fields": null
+      }
+    },
+    {
+      "field_key": "0x7eb4036673c8611e43c3eff1202446612f22a4b3bac92b7e14c0562ade5f1a3f",
+      "state": {
+        "id": "0x48954635d648a30c92751ebff4a6ee54bd8edfa3a97e7103c75f5cfd199c27a77eb4036673c8611e43c3eff1202446612f22a4b3bac92b7e14c0562ade5f1a3f",
+        "owner": "rooch1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqhxqaen",
+        "owner_bitcoin_address": null,
+        "flag": 0,
+        "state_root": "0x5350415253455f4d45524b4c455f504c414345484f4c4445525f484153480000",
+        "size": "0",
+        "created_at": "1732745942851",
+        "updated_at": "1732745942851",
+        "object_type": "0x2::object::DynamicField<u64, 0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>>",
+        "value": "0x0100000000000000013f10b4aeb367801d085a02a173a3c1952232e70dd52feee71de5d05b0b3d317f",
+        "decoded_value": {
+          "abilities": 12,
+          "type": "0x2::object::DynamicField<u64, 0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>>",
+          "value": {
+            "name": "1",
+            "value": {
+              "abilities": 12,
+              "type": "0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>",
+              "value": {
+                "id": "0x3f10b4aeb367801d085a02a173a3c1952232e70dd52feee71de5d05b0b3d317f"
+              }
+            }
+          }
+        },
+        "display_fields": null
+      }
+    },
+    {
+      "field_key": "0xf30b43fa7dbdf31380c5b3efb2e960569b6caa30e6ee302358fbd43a605a5dbc",
+      "state": {
+        "id": "0x48954635d648a30c92751ebff4a6ee54bd8edfa3a97e7103c75f5cfd199c27a7f30b43fa7dbdf31380c5b3efb2e960569b6caa30e6ee302358fbd43a605a5dbc",
+        "owner": "rooch1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqhxqaen",
+        "owner_bitcoin_address": null,
+        "flag": 0,
+        "state_root": "0x5350415253455f4d45524b4c455f504c414345484f4c4445525f484153480000",
+        "size": "0",
+        "created_at": "1732745942851",
+        "updated_at": "1732745942851",
+        "object_type": "0x2::object::DynamicField<u64, 0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>>",
+        "value": "0x030000000000000001ec994759352cb6a348ae1755f7bcadac438d92180fd911af0cf422c70319a167",
+        "decoded_value": {
+          "abilities": 12,
+          "type": "0x2::object::DynamicField<u64, 0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>>",
+          "value": {
+            "name": "3",
+            "value": {
+              "abilities": 12,
+              "type": "0x2::object::Object<0x24840092564334fb3f5c63a864ff9c13bbdafaab906f48ee7178f26a3dc8554b::pond::PondState>",
+              "value": {
+                "id": "0xec994759352cb6a348ae1755f7bcadac438d92180fd911af0cf422c70319a167"
+              }
+            }
+          }
+        },
+        "display_fields": null
+      }
+    }
+  ],
+  "next_cursor": "0xf30b43fa7dbdf31380c5b3efb2e960569b6caa30e6ee302358fbd43a605a5dbc",
+  "has_next_page": false
+}
+*/
 
 /*
 testnet
